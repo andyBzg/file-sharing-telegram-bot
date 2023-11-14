7976178e91997288733ade5077b717bf3b2903ef
andyBzg <andr.bzg@gmail.com>
2023-11-14 16:21:57 +0100

    feat: Added RabbitMQ support - Added Telegram dependencies and configurations - Added RabbitMQ configuration and queues - Added Telegram Update processing features

---
+++ dispatcher/src/main/java/com/andybzg/dispatcher/service/impl/UpdateProducerImpl.java
@@ -0,0 +1,24 @@
+package com.andybzg.dispatcher.service.impl;
+
+import com.andybzg.dispatcher.service.UpdateProducer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Service
+@Slf4j
+public class UpdateProducerImpl implements UpdateProducer {
+
+    private final RabbitTemplate rabbitTemplate;
+
+    public UpdateProducerImpl(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Override
+    public void produce(String rabbitQueue, Update update) {
+        log.debug(update.getMessage().getText());
+        rabbitTemplate.convertAndSend(rabbitQueue, update);
+    }
+}
