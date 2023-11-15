1372ce95c263b700e2fb8f1d6d6b82ccff08281a
andyBzg <andr.bzg@gmail.com>
2023-11-15 20:26:53 +0100

    feat: Added new module for persisting telegram user to database

---
+++ common-jpa/src/main/java/com/andybzg/enums/UserState.java
@@ -0,0 +1,6 @@
+package com.andybzg.enums;
+
+public enum UserState {
+    BASIC_STATE,
+    WAIT_FOR_EMAIL_STATE
+}
--- node/src/main/java/com/andybzg/service/impl/MainServiceImpl.java
+++ node/src/main/java/com/andybzg/service/impl/MainServiceImpl.java
@@ -1,34 +1,60 @@
 package com.andybzg.service.impl;
 
+import com.andybzg.dao.AppUserDAO;
 import com.andybzg.dao.RawDataDAO;
+import com.andybzg.entity.AppUser;
 import com.andybzg.entity.RawData;
+import com.andybzg.enums.UserState;
 import com.andybzg.service.MainService;
 import com.andybzg.service.ProducerService;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 
 @Service
 public class MainServiceImpl implements MainService {
 
     private final RawDataDAO rawDataDAO;
     private final ProducerService producerService;
+    private final AppUserDAO appUserDAO;
 
-    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService) {
+    public MainServiceImpl(RawDataDAO rawDataDAO, ProducerService producerService, AppUserDAO appUserDAO) {
         this.rawDataDAO = rawDataDAO;
         this.producerService = producerService;
+        this.appUserDAO = appUserDAO;
     }
 
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
+        Message textMessage = update.getMessage();
+        User telegramUser = textMessage.getFrom();
+        AppUser appUser = findOrSaveAppUser(telegramUser);
 
         Message message = update.getMessage();
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(message.getChatId());
         sendMessage.setText("Hello from NODE");
         producerService.produceAnswer(sendMessage);
+    }
+
+    private AppUser findOrSaveAppUser(User telegramUser) {
+        AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
+        if (persistentAppUser == null) {
+            AppUser transientAppUser = AppUser.builder()
+                    .telegramUserId(telegramUser.getId())
+                    .username(telegramUser.getUserName())
+                    .firstName(telegramUser.getFirstName())
+                    .lastname(telegramUser.getLastName())
+                    //TODO change default active state after implementing registration feature
+                    .isActive(true)
+                    .state(UserState.BASIC_STATE)
+                    .build();
+            return appUserDAO.save(transientAppUser);
+        }
+        return persistentAppUser;
     }
 
     private void saveRawData(Update update) {
