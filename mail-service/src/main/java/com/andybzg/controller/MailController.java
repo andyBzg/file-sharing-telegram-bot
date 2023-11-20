5f0043cf71d8d1eae29e90d9814495e3fb4d31b3
andyBzg <andr.bzg@gmail.com>
2023-11-20 18:58:21 +0100

    refactor: Mail-service module - added Lombok constructor annotations - reordered annotations

--- mail-service/src/main/java/com/andybzg/controller/MailController.java
+++ mail-service/src/main/java/com/andybzg/controller/MailController.java
@@ -2,21 +2,19 @@ package com.andybzg.controller;
 
 import com.andybzg.dto.MailParams;
 import com.andybzg.service.MailSenderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RequiredArgsConstructor
 @RequestMapping("/mail")
+@RestController
 public class MailController {
 
     private final MailSenderService mailSenderService;
-
-    public MailController(MailSenderService mailSenderService) {
-        this.mailSenderService = mailSenderService;
-    }
 
     @PostMapping("/send")
     public ResponseEntity<?> sendActivationMail(@RequestBody MailParams mailParams) {
