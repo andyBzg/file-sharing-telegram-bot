afd3a1953aa0310be4f329cc721114a7270265d8
andyBzg <andr.bzg@gmail.com>
2023-11-24 16:38:43 +0100

    refactor: Small fix for proper execution of GitHub Actions

---
+++ dispatcher/src/test/java/com/andybzg/dispatcher/DispatcherApplicationTest.java
@@ -0,0 +1,13 @@
+package com.andybzg.dispatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatcherApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
