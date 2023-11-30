ca397811a2fb93f5a713336b6cdf0db151397713
andyBzg <andr.bzg@gmail.com>
2023-11-30 13:57:14 +0100

    test: Add H2 database - added H2 database dependency for tests - added test/resources/application.properties with H2 settings

---
+++ rest-service/src/test/java/com/andybzg/RestServiceTest.java
@@ -0,0 +1,13 @@
+package com.andybzg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
